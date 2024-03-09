b7f50287c75d05f69f4d4f737adce5fe033fe759
anup <anupmandal828109@gmail.com>
2024-03-10 04:09:31 +0530

    ok

--- app/views/signin.py
+++ app/views/signin.py
@@ -8,30 +8,35 @@ from django.contrib import messages
 # Create your views here.
 
 def signin(request):
-    
-    username="anurag"
-    password="anurag"
-    try:
-        users=User.objects.get(username=username)  
-        if users.delete == False:
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('dasboard')
-            else:
-                messages.info(request, 'Check Password !')
-                return redirect('signin')
-        else:
-            messages.info(request, 'User Not Register ')
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        try:
+            users=User.objects.get(username=username)  
+            try:
+                user = authenticate(request, username=username, password=password)
+                print(12)
+                if user is not None:
+                    login(request, user)
+                    print(545)
+                    return redirect('upload_csv')
+                else:
+                    messages.info(request, 'Check Password !')
+                    return redirect('signin')
+            except:
+                print(78454)
+                messages.info(request, 'User Not Register ')
+                return redirect('signup')
+        except:
+            print(12)
+            messages.info(request, 'User Not Register !')
             return redirect('signup')
-    except:
-        messages.info(request, 'User Not Register !')
-        return redirect('/')
+    else:
+        return render(request,'signin.html')
 
 
-
-# @login_required(login_url='signin')
-# def signout(request):
-#     logout(request)
-#     # messages.info(request, "Three credits remain in your account.")
-#     return redirect('home')
+@login_required(login_url='signin')
+def signout(request):
+    logout(request)
+    # messages.info(request, "Three credits remain in your account.")
+    return redirect('/')
